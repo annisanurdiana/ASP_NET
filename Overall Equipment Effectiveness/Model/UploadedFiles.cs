62a6e0d2e562aaea089aab294721be8c1518f56b
Diana <75033936+annisanurdiana@users.noreply.github.com>
2024-03-15 08:44:30 +0700

    Add files via upload

---
+++ Overall Equipment Effectiveness/Model/UploadedFiles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OEE_SSC.Models
+{
+    public class UploadedFiles
+    {
+        public string FileN { get; set; }
+        public string FilePath { get; set; }
+        public HttpPostedFileBase UploadFile { get; set; }
+        public List<UploadedFiles> lstUploadedFiles { get; set; }
+    }
+}
