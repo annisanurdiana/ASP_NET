fb936f0fcc54b01bdad17b7d71dcc5f7f943d72b
Diana <75033936+annisanurdiana@users.noreply.github.com>
2024-03-15 08:41:35 +0700

    Add files via upload

---
+++ Electronic Scrap/Model/pimScrabData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OEE_SSC.Models
+{
+    public class pimScrabData
+    {
+        // new PIM table
+        public IEnumerable<pim_scrap> Pim_Scraps { get; set; }
+
+    }
+}
